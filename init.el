8b2a73fe89658f60a76789f096e01e84363f4262
Ira Cooper <ira@wakeful.net>
2024-05-20 00:49:57 -0400

    Initial emacs config start.

---
+++ init.el
@@ -0,0 +1,23 @@
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(require 'init-elpa)
+(require 'init-ui)
+(require 'init-editing)
+(require 'init-company-mode)
+(require 'init-which-key)
+(require 'init-helm)
+
+(provide 'init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(helm-ag helm-M-x help-M-x helm-ls-git helm golden-ratio atom-one-dark-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
