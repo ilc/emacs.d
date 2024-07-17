2693a889aed12732dd47d3e744b04c38d8fd9ab1
Ira Cooper <ira@wakeful.net>
2024-07-17 17:02:24 -0400

    terraform: add basic terraform support.

--- init.el
+++ init.el
@@ -15,7 +15,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-ui ccls helm-ag helm-M-x help-M-x helm-ls-git helm golden-ratio atom-one-dark-theme)))
+   '(company-terraform terraform-doc terraform-mode lsp-ui ccls helm-ag helm-M-x help-M-x helm-ls-git helm golden-ratio atom-one-dark-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
